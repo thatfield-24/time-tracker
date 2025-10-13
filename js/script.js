44326ae64109a2dab0960a6784c0d877c2d9e2f2
Tristan Hatfield <66078555+thatfield-24@users.noreply.github.com>
2025-10-13 16:21:57 -0500

    Base App Creation
    Created the basic functions of the app.
    Calculate Hours of Free time left from user input.
    Made basic javascript for backend. Simple CSS for page design at the moment.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function init(){
+//add your javascript between these two lines of code
+ var button = document.getElementById('calculate');
+ 
+ function textAlert(){
+     var sleep = document.getElementById('sleep');
+	 var classes = document.getElementById('classes');
+     var homework = document.getElementById('homework');
+     var fitness = document.getElementById('fitness');
+     var travel = document.getElementById('travel');
+     var freeTime = (168 - (sleep.value * 7) - classes.value - homework.value - fitness.value - travel.value)
+    document.getElementById('textoutput').innerHTML = "Free Time Hours Left: " + freeTime;
+
+ }
+ 
+ button.addEventListener('click', textAlert)
+}
+
+window.addEventListener('load', init);
+
